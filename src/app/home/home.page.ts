75ab40ef94798fde887b490f5a03e4a6f504db65
Verisimo Erazo <verisimodanielerazo@gmail.com>
2022-07-15 16:54:24 -0500

    Actividad 1 V.E

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOpt = {
+    initialslide: 0, //slide incial
+    slidesPerView: 1, //slide por vista
+    centeredSlides: true, //que las slides esten centradas
+    speed: 400 //velocida de transicion de cada slide en milisegundo
+  }
+
   constructor() {}
 
 }
